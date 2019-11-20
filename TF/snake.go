25e7c636925bd081ff74cd2f9fe7131d90113a30
Lucas Félix <lucas.salaverry@edu.pucrs.br>
2019-11-20 08:37:14 -0300

    Functional two player movement

--- TF/snake.go
+++ TF/snake.go
@@ -1,24 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"container/list"
 	"fmt"
+	"math/rand"
 	"os"
-	"time"
 	"os/exec"
 	"strconv"
-	"container/list"
-	"math/rand"
+	"sync"
+	"time"
 
 	term "github.com/nsf/termbox-go"
 )
 
 type Pair struct {
-    a, b interface{}
+	a, b interface{}
 }
 
 const SIZE int = 20
 
 var field [SIZE][SIZE]string
+
+var startGame sync.Mutex
+var updateGame sync.Mutex
+var notifyUpdate sync.Mutex
 
 const PLAYERS int = 2
 
@@ -53,82 +59,89 @@ func spawnFruit() {
 		x := r.Intn(SIZE)
 		y := r.Intn(SIZE)
 		field[y][x] = " F "
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 
 }
 
-func snakeController(id int, inputs <-chan rune) {
+func snakeController(id int, inputs chan rune, up rune, down rune, left rune, right rune) {
+
 	name := " " + strconv.Itoa(id) + " "
 
-	headX := 5
-	headY := 5
+	headX := (SIZE / (PLAYERS + 1)) * id
+	headY := (SIZE / (PLAYERS + 1)) * id
 
 	field[headY][headX] = name
 	body := list.New()
 
-
+	startGame.Lock()
+	if id != PLAYERS {
+		startGame.Unlock()
+	}
+	fmt.Println("Cobra" + strconv.Itoa(id) + "Desbloquada")
 	for {
+		//update := false
 		key := <-inputs
-		
-		if(body.Len() > 0){
+
+		if body.Len() > 0 {
 			change := body.Back()
 			field[change.Value.(Pair).a.(int)][change.Value.(Pair).b.(int)] = " _ "
 			body.Remove(change)
-			aux := Pair{headY,headX}
+			aux := Pair{headY, headX}
 			body.PushFront(aux)
 		}
 
 		switch key {
-		case rune(119):
+		case up:
 
 			field[headY][headX] = " _ "
 
 			headY--
 
-
-		case rune(97):
+		case left:
 
 			field[headY][headX] = " _ "
 
 			headX--
-		
 
-		case rune(115):
+		case down:
 
 			field[headY][headX] = " _ "
 
 			headY++
-			
 
-		case rune(100):
+		case right:
 
 			field[headY][headX] = " _ "
 
 			headX++
-		
 
+		default:
+			inputs <- key
+			//updateGame.Lock()
+			continue
 		}
 
-		if field[headY][headX] == " F "{
-			point := Pair{headY,headX}
+		if field[headY][headX] == " F " {
+			point := Pair{headY, headX}
 			body.PushFront(point)
 		}
 
 		field[headY][headX] = name
 
 		for e := body.Front(); e != nil; e = e.Next() {
-			field[e.Value.(Pair).a.(int)][e.Value.(Pair).b.(int)] = name 
+			field[e.Value.(Pair).a.(int)][e.Value.(Pair).b.(int)] = name
 		}
 
+		updateGame.Lock()
 		drawGame()
 	}
 
 }
 
 func drawGame() {
-	cmd := exec.Command("clear");//Linux
-	//cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+	//cmd := exec.Command("clear");//Linux
+	cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 	for rows := 0; rows < SIZE; rows++ {
@@ -141,6 +154,7 @@ func drawGame() {
 		}
 		fmt.Print("\n")
 	}
+	updateGame.Unlock()
 }
 
 func sendInput(input string, inputs chan<- string) {
@@ -149,13 +163,42 @@ func sendInput(input string, inputs chan<- string) {
 	}
 }
 func main() {
-	inputs := make(chan rune, 100)
+	inputs := make(chan rune)
+
+	startGame.Lock()
+
+	reader := bufio.NewReader(os.Stdin)
+	for index := 1; index < PLAYERS+1; index++ {
+		fmt.Println("Jogador " + strconv.Itoa(index))
+		fmt.Println("Digite a tecla que usará para ir para cima.")
+		up, _, _ := reader.ReadRune()
+		reader.Reset(os.Stdin)
+
+		fmt.Println("Digite a tecla que usará para ir para a esquerda.")
+		left, _, _ := reader.ReadRune()
+		reader.Reset(os.Stdin)
+
+		fmt.Println("Digite a tecla que usará para ir para baixo.")
+		down, _, _ := reader.ReadRune()
+		reader.Reset(os.Stdin)
+
+		fmt.Println("Digite a tecla que usará para ir para a direita.")
+		right, _, _ := reader.ReadRune()
+		reader.Reset(os.Stdin)
+
+		go snakeController(index, inputs, up, down, left, right)
+	}
+
 	go treatInput(inputs)
 
+	updateGame.Lock()
 	drawGame()
-	
+	//Alterar isso para ocorrer apenas depois que todas cobrinhas se "Desbloquarem"
+
 	go spawnFruit()
-	go snakeController(0, inputs)
+
+	startGame.Unlock()
+
 	for {
 
 	}
