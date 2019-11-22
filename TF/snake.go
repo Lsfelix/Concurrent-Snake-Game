5310b69a6a9dd08c5da1f3cbace9d6f5c778f2f9
Lucas Félix <lucas.salaverry@edu.pucrs.br>
2019-11-22 18:59:57 -0300

    Solving concurrent issues

--- TF/snake.go
+++ TF/snake.go
@@ -24,13 +24,11 @@ var field [SIZE][SIZE]string
 
 var startGame sync.Mutex
 var updateGame sync.Mutex
-var notifyUpdate sync.Mutex
 
-const PLAYERS int = 2
+const PLAYERS int = 3
 
 func treatInput(inputs chan<- rune) {
 	term.Init()
-	defer term.Close()
 
 	for {
 		switch ev := term.PollEvent(); ev.Type {
@@ -38,14 +36,12 @@ func treatInput(inputs chan<- rune) {
 			switch ev.Key {
 
 			case term.KeyEsc:
+				term.Close()
 				os.Exit(3)
 			}
-
-			//fmt.Println(ev.Ch)
 			inputs <- ev.Ch
 
 		case term.EventError:
-			fmt.Println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 			panic(ev.Err)
 		}
 
@@ -54,12 +50,15 @@ func treatInput(inputs chan<- rune) {
 }
 
 func spawnFruit() {
-	r := rand.New(rand.NewSource(99))
+	r := rand.New(rand.NewSource(13))
 	for {
 		x := r.Intn(SIZE)
 		y := r.Intn(SIZE)
+		if field[y][x] != " _ " {
+			continue
+		}
 		field[y][x] = " F "
-		time.Sleep(10 * time.Second)
+		time.Sleep(3 * time.Second)
 	}
 
 }
@@ -78,18 +77,13 @@ func snakeController(id int, inputs chan rune, up rune, down rune, left rune, ri
 	if id != PLAYERS {
 		startGame.Unlock()
 	}
-	fmt.Println("Cobra" + strconv.Itoa(id) + "Desbloquada")
+	//fmt.Println("Cobra" + strconv.Itoa(id) + "Desbloquada")
 	for {
 		//update := false
 		key := <-inputs
 
-		if body.Len() > 0 {
-			change := body.Back()
-			field[change.Value.(Pair).a.(int)][change.Value.(Pair).b.(int)] = " _ "
-			body.Remove(change)
-			aux := Pair{headY, headX}
-			body.PushFront(aux)
-		}
+		oldHeadX := headX
+		oldHeadY := headY
 
 		switch key {
 		case up:
@@ -122,6 +116,24 @@ func snakeController(id int, inputs chan rune, up rune, down rune, left rune, ri
 			continue
 		}
 
+		updateGame.Lock()
+
+		if (field[headY][headX] != " _ " && field[headY][headX] != " F ") || headX < 0 || headX > SIZE || headY < 0 || headY > SIZE {
+			for e := body.Front(); e != nil; e = e.Next() {
+				field[e.Value.(Pair).a.(int)][e.Value.(Pair).b.(int)] = " _ "
+			}
+			updateGame.Unlock()
+			break
+		}
+
+		if body.Len() > 0 {
+			change := body.Back()
+			field[change.Value.(Pair).a.(int)][change.Value.(Pair).b.(int)] = " _ "
+			body.Remove(change)
+			aux := Pair{oldHeadY, oldHeadX}
+			body.PushFront(aux)
+		}
+
 		if field[headY][headX] == " F " {
 			point := Pair{headY, headX}
 			body.PushFront(point)
@@ -133,15 +145,14 @@ func snakeController(id int, inputs chan rune, up rune, down rune, left rune, ri
 			field[e.Value.(Pair).a.(int)][e.Value.(Pair).b.(int)] = name
 		}
 
-		updateGame.Lock()
 		drawGame()
 	}
 
 }
 
 func drawGame() {
-	//cmd := exec.Command("clear");//Linux
-	cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+	cmd := exec.Command("clear") //Linux
+	//cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 	for rows := 0; rows < SIZE; rows++ {
@@ -162,6 +173,7 @@ func sendInput(input string, inputs chan<- string) {
 		inputs <- input
 	}
 }
+
 func main() {
 	inputs := make(chan rune)
 
@@ -194,7 +206,6 @@ func main() {
 	updateGame.Lock()
 	drawGame()
 	//Alterar isso para ocorrer apenas depois que todas cobrinhas se "Desbloquarem"
-
 	go spawnFruit()
 
 	startGame.Unlock()
